6451019eb5a876ca32d104e33ca820ef9a423e29
Charlotte Demaret <demaret.cha@gmail.com>
2021-09-27 13:07:52 +0900

    navbar redirecting but active class doesn't last

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,1 +1,29 @@
-import 'bootstrap';
+import bootstrap from 'bootstrap';
+
+// var triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'))
+// triggerTabList.forEach(function (triggerEl) {
+//   var tabTrigger = new bootstrap.Tab(triggerEl)
+//   triggerEl.addEventListener('click', function (event) {
+//     event.preventDefault()
+//     tabTrigger.show()
+//   })
+// });
+
+var triggerTabList = document.querySelectorAll('#myTab a')
+console.log(triggerTabList);
+triggerTabList.forEach((item) => {
+  item.addEventListener("click", (event)=> {
+    console.log(event.currentTarget);
+    event.currentTarget.classList.add("active");
+  });
+});
+
+// var triggerEl = document.querySelector('#myTab a[href="#profile"]')
+// bootstrap.Tab.getInstance(triggerEl).show() // Select tab by name
+
+//   var tabTrigger = new bootstrap.Tab(triggerEl)
+//   triggerEl.addEventListener('click', function (event) {
+//     event.preventDefault()
+//     tabTrigger.show()
+//   })
+// })
